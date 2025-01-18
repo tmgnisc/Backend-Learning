b3e00976e0eaeb3641f4c5ed59eb777f977b9727
Nischal <tamangnischal2018@gmail.com>
2025-01-18 18:52:56 +0545

    backend setup completed

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+
+const app = express()
+
+app.use(bodyParser.json())
+dotenv.config()
+const PORT = process.env.PORT || 5000;
+const MONGOURI = process.env.MONGO_URI;
+mongoose.connect(MONGOURI).then(()=>{
+    console.log("database connected succesfully")
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch((error)=>console.log(error))
+
