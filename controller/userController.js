d1da30e1a18c9c9b3888b0b5362695237b8c0d58
Nischal <tamangnischal2018@gmail.com>
2025-01-19 13:14:58 +0545

    update api completed

--- controller/userController.js
+++ controller/userController.js
@@ -19,7 +19,26 @@ export const create = async (req, res)=>{
 
 export const fetch = async (req, res)=>{
     try {
-        return res.json("Hello world")
+      const users = await User.find();
+      if(users.length === 0){
+        return res.status(404).json({ message: "User not found"});
+      }
+      res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+
+export const update = async(req, res)=>{
+    try {
+          const id = req.params.id;
+          const userExist = await User.findOne({_id: id})
+          if(!userExist){
+            return res.status(404).json({ message: "User not found"});
+          }
+          const updateUser = await User.findByIdAndUpdate(id, req.body, {new:true})
+          res.status(201).json(updateUser)
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
     }
