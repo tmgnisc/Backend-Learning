4117de835121fc8d846026974901c667b5099de0
Nischal <tamangnischal2018@gmail.com>
2025-01-19 12:59:17 +0545

    data posted in db successfully

--- controller/userController.js
+++ controller/userController.js
@@ -4,13 +4,13 @@ import User from "../model/userModel.js"
 export const create = async (req, res)=>{
     try {
         const userData = new User(req.body);
-        const {email}=userData;
-        const userExist = await User.findOne({email});
+        const { email } = userData;
+        const userExist = await User.findOne({ email });
         if(userExist){
             return res.status(400).json({message: "user already exists"})
         }
         const savedUser = await userData.save();
-        res.status(200),json(savedUser);
+        res.status(200).json(savedUser);
     } catch (error) {
         res.status(500).json({error: "Internal server error"})
         
